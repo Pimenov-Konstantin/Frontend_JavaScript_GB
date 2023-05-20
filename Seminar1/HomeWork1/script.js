b7f4871327c372160b790c86b693a88f103d7315
KonPim <100049831+Pimenov-Konstantin@users.noreply.github.com>
2023-05-20 21:23:41 +0300

    Add files via upload

---
+++ Seminar1/HomeWork1/script.js
@@ -0,0 +1,9 @@
+// Task 1
+const celsius = 10;
+const fahrenheit = (9 / 5) * 10 + 32;
+console.log(celsius + ' Из Цельсия в Градусы Фаренгейта: ' + fahrenheit);
+
+// Task 2
+const name = 'Kostya';
+const admin = name;
+console.log(admin);
---
+++ Seminar1/script.js
@@ -0,0 +1,50 @@
+// Task 1
+console.log('Текст из script.js');
+// Task 2
+const num = 123;
+console.log(num);
+
+let a = 5;
+a = 7;
+console.log(a);
+
+const b = 1 + 2 + 3;
+console.log(b);
+
+// это первый комментарий
+/* А это
+уже
+второй */
+
+// Task 3
+const c = 10;
+const d = 2;
+const summ = c + d;
+const difference = c - d;
+const multiplication = c * d;
+const division = c / d;
+console.log(`Сумма ${summ}, Разность ${difference}, Умножение ${multiplication}, Деление ${division}`);
+
+const summNum = summ + difference + multiplication + division;
+console.log(`Сумма всех значение: ${summNum}`);
+
+// Task 5
+const f = 1.5;
+const e = 0.75;
+console.log(`f + e = ${f+e}`);
+const g = 576 * '1';
+console.log(-g + 11);
+
+// Task 6
+const myName = 'Kostya';
+const surName = 'Pimenov';
+console.log(myName);
+console.log(surName);
+
+const java = 'Java';
+const scriptjs = 'Script';
+console.log(`Full name ${java + scriptjs}`);
+
+const hello = 'hello';
+const world = 'world';
+console.log(hello + ', ' + world + '!');
